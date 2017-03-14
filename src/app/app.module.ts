6e3ca9b722652b302ab64aab2fe2007b9825bdbb
miles hill <miles1618@gmail.com>
2017-03-14 06:55:40 -0500

    App setup. Connected to Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,25 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// Firebase
+import { AngularFireModule } from 'angularfire2';
+
+// Components
 import { AppComponent } from './app.component';
+
+// Styinling; Foundation 6
+
+
+// Firebase config
+export const firebaseConfig = {
+  apiKey: "AIzaSyD-Ii3gI1wWh8JqGaq0IcTk9iPT4KPN1kY",
+  authDomain: "businesscontacts-7870d.firebaseapp.com",
+  databaseURL: "https://businesscontacts-7870d.firebaseio.com",
+  storageBucket: "businesscontacts-7870d.appspot.com",
+  messagingSenderId: "932974638403"
+};
+
+
 
 @NgModule({
   declarations: [
@@ -12,7 +30,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
